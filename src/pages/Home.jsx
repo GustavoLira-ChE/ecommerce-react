662aacf5286e581785f65716bbef1486b6ca5c3a
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-06 16:18:09 -0500

    feat: Created Header componente and its style file, imported to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
     return(
-        <h1>
-            Ecommerce React
-        </h1>
+        <div>
+            <Header />
+        </div>
     );
 }
 
