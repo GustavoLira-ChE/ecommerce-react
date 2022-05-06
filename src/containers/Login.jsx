6a3e16a1d5a9193b9b2010379310efabcda94b79
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-06 12:21:07 -0500

    style: change some names

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -6,14 +6,14 @@ const Login = () => {
         <div className="login">
             <div className="form-container">
                 <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-                <h1 className="title">Create a new password</h1>
-                <p className="subtitle">Enter a new password for you account</p>
+                <h1 className="title"></h1>
+                <p className="subtitle">Login to EcommerceReact</p>
                 <form action="/" className="form">
-                    <label for="password" className="label">Password</label>
-                    <input type="password" id="password" placeholder="*********" className="input input-password" />
+                    <label for="password" className="label">Email</label>
+                    <input type="password" id="password" placeholder="user@email.xyz" className="input input-password" />
                     <label for="new-password" className="label">Password</label>
                     <input type="password" id="new-password" placeholder="*********" className="input input-password" />
-                    <input type="submit" value="Confirm" className="primary-button login-button" />
+                    <input type="submit" value="Login" className="primary-button login-button" />
                 </form>
             </div>
         </div>
