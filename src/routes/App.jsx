4e7e3f8697901e50c608c7c9a8933a0085527a70
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-06 12:22:30 -0500

    feat: use of Routes to navigate

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Layout from "../containers/Layout";
 import Login from "../containers/Login";
 import RecoveryPassword from "../containers/RecoveryPassword";
@@ -6,10 +7,17 @@ import '../styles/global.css';
 
 const App = () =>{
     return(
-        <Layout>
-            <Login />
-            <RecoveryPassword />
-        </Layout>
+        <BrowserRouter>
+            <Routes>
+                <Layout>
+                    <Route exact path="/home" component={Home} />
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/recovery-password" component={RecoveryPassword}/>
+                    <Route component={NotFoud}/>
+                </Layout>
+            </Routes>
+        </BrowserRouter>
+        
     );
 }
 
