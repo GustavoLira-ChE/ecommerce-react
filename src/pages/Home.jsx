fcf98724e74b302ff42edde5d61896b7e67cce43
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-07 09:27:02 -0500

    feat: added product list component to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import ProductList from "../components/ProductList";
 
 const Home = () => {
     return(
         <div>
             <Header />
+            <ProductList />
         </div>
     );
 }
