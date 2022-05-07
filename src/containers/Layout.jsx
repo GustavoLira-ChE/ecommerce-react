94ebba15a5c45e7559a9ea2f0e8df88b6d1d015c
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-07 09:26:21 -0500

    add: temporal links to navigate between pages

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -4,7 +4,19 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
     return(
         <div className="Layout">
-            
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/recovery">Recovery Password</Link>
+                    </li>
+                </ul>
+            </nav>
             <Outlet />
         </div>
     );
