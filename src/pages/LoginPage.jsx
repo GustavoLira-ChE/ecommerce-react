a8ed998ed9cf434f32f62bdad38a3391de676c00
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-11 14:35:00 -0500

    add: logo page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import Login from "../containers/Login";
+
+const LoginPage = () => {
+    return(
+        <div>
+            <Header />
+            <Login />
+        </div>
+    )
+};
+
+export default LoginPage;
