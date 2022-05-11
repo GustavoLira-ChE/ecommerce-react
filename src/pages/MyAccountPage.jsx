36a388f7f85f45094d70697efb7a3bf5d92b5273
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-11 15:38:10 -0500

    add: MyAccount page

---
+++ src/pages/MyAccountPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import MyAccount from "../components/MyAccount";
+
+const MyAccountPage = () => {
+    return(
+        <div>
+            <Header />
+            <MyAccount />
+        </div>
+    );
+};
+
+export default MyAccountPage;
