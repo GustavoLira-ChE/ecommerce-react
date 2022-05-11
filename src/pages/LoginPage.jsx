cae8623613096c164d1d84557abc65c0cbf6ebcc
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-11 15:28:05 -0500

    fix: import route because some file was moved from containers to components

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import Login from "../containers/Login";
+import Login from "../components/Login";
 
 const LoginPage = () => {
     return(
--- src/pages/RecoveryPasswordVerificationPage.jsx
+++ src/pages/RecoveryPasswordVerificationPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import RecoveryPasswordVerification from "../containers/RecoveryPasswordVerification";
+import RecoveryPasswordVerification from "../components/RecoveryPasswordVerification";
 
 const RecoveryPasswordVerificationPage = () => {
     return(
