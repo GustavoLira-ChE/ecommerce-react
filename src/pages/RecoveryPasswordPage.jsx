598f08a0e7863c3cc6ca55ed82e051b988b4f095
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-11 15:05:13 -0500

    add: Recovery password pages

---
+++ src/pages/RecoveryPasswordPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import RecoveryPassword from "../containers/RecoveryPassword";
+
+const RecoveryPasswordPage = () => {
+    return(
+        <div>
+            <Header />
+            <RecoveryPassword />
+        </div>
+    );
+};
+
+export default RecoveryPasswordPage;
---
+++ src/pages/RecoveryPasswordVerificationPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import RecoveryPasswordVerification from "../containers/RecoveryPasswordVerification";
+
+const RecoveryPasswordVerificationPage = () => {
+    return(
+        <div>
+            <Header />
+            <RecoveryPasswordVerification />
+        </div>
+    );
+};
+
+export default RecoveryPasswordVerificationPage;
