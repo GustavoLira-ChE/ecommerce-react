a0c9add962b159d1d71e70c0bb23fbe782deb1a5
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-12 12:06:52 -0500

    add: Mu order component

---
+++ src/components/MyOrder.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import OrderSummary from '../containers/OrderSummary';
+import ShoppingCard from '../containers/ShoppingCard';
+import "../styles/myOrder.scss";
+
+const MyOrder = () => {
+    return(
+        <div className="my-order">
+            <div className="my-order-container">
+            <h1 className="title">My order</h1>
+            <OrderSummary />
+            <ShoppingCard />
+            <ShoppingCard />
+            <ShoppingCard />
+            <ShoppingCard />
+    </div>
+  </div>
+    );
+};
+
+export default MyOrder;
