4a9c6724fc1d844da6dfa35b4b9725da9496ca41
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-12 17:01:04 -0500

    feat: added hooks to show and hide menu options and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,27 @@
 import React, {useState} from "react";
 import DesktopMenu from "./DesktopMenu";
-import './../styles/header.scss'
+import MobileMenu from "./MobileMenu";
+import './../styles/header.scss';
 
 const Header = () => {
-	const [toggle, setToggle] =useState(false);
-	const handleToggle = () => {
-		setToggle(!toggle);
+	const [toggleDesktop, setToggleDesktop] =useState(false);
+	const [toggleMobile, setToggleMobile] = useState(false);
+	const handleToggleDesktop = () => {
+		setToggleDesktop(!toggleDesktop);
+		setToggleMobile(false);
+	};
+	const handleToggleMobile = () => {
+		setToggleMobile(!toggleMobile);
+		setToggleDesktop(false);
 	};
     return(
         <nav>
 			<div className="navbar-left">
 				<img src="./assets/logos/logo_yard_sale.svg" alt="menu" className="menu" />
-				<img src="./assets/icons/icon_menu.svg" alt="celphone icon" className="menu-icon"/>
+				<figure onClick={handleToggleMobile}>
+					<img src="./assets/icons/icon_menu.svg" alt="celphone icon" className="menu-icon"/>
+				</figure>
+				{toggleMobile && <MobileMenu />}
 			</div>
 			<div className="navbar-center">
 				<img src="./assets/logos/logo_yard_sale.svg" alt="logo" className="logo" />
@@ -38,14 +48,14 @@ const Header = () => {
 			</div>
 			<div className="navbar-right">
 				<ul>
-					<li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
+					<li className="navbar-email" onClick={handleToggleDesktop}>platzi@example.com</li>
 					<li className="navbar-shopping-cart">
 						<img src="./assets/icons/icon_shopping_cart.svg" alt="shopping cart" />
 						<div>2</div>
 					</li>
 				</ul>
 			</div>
-			{toggle && <DesktopMenu />}
+			{toggleDesktop && <DesktopMenu />}
 		</nav>
     );
 }
