f05e125e03257119d8b654ac55a6f5c7e242a1c6
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-12 11:45:02 -0500

    add: order Summary container

---
+++ src/containers/OrderSummary.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/oderSummary.scss";
+
+const OrderSummary = () => {
+    return(
+        <div class="my-order-content">
+            <div class="order">
+            <p>
+                <span>03.25.21</span>
+                <span>6 articles</span>
+            </p>
+            <p>$560.00</p>
+            </div>
+      </div>
+    );
+};
+
+export default OrderSummary;
