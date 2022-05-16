0756a565666449eae65ecbe3c3ff3b758df242c8
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-16 16:40:01 -0500

    fix: remove MyOrderDetails container

--- src/pages/MyOrderPage.jsx
+++ src/pages/MyOrderPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "../components/Header";
-import MyOrderDetails from "../containers/MyOrderDetails";
 import MyOrder from "../containers/MyOrder";
 
 const MyOrderPage = () => {
     return(
         <div>
             <Header />
-            <MyOrderDetails />
             <MyOrder />
         </div>
     );
