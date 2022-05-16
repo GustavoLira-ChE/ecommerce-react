fc9cbbf12d3eee02fbca9b7b97e1ae9a765ba8b1
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-16 11:21:23 -0500

    feat: handle products from an API and render them

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,18 @@
 import React, {useState} from "react";
 import './../styles/productcard.scss'
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
     const [cart, setCart] = useState([]);
     const handleClick = () => {
         setCart([]);
     }
     return(
         <div className="product-card">
-            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <img src={product.images} alt={product.title} />
             <div className="product-info">
                 <div>
-                    <p>$120,00</p>
-                    <p>Bike</p>
+                    <p>${product.price}</p>
+                    <p>{product.title}</p>
                 </div>
                 <figure onClick={handleClick}>
                     <img src="./assets/icons/bt_add_to_cart.svg" alt=""/>
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
 import './../styles/productlist.scss';
+import useGetProducts from "../hooks/useGetProducts";
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+
+    const products = useGetProducts(API);
+    
     return(
         <section className="main-container">
             <div className="cards-container">
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+                {products.map(product => (
+                    <ProductCard product={product} key={product.id}/>
+                ))}
             </div>
         </section>
     );
