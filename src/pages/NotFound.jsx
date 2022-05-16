262da20a0b73f2fa4f6be0bbf4fa966e120a8150
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-16 12:40:42 -0500

    styles: added styles to NotFound page and add header component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "../styles/notFound.scss"
+import Header from "../components/Header"
 
 const NotFound = () => {
     return(
         <div>
-            Not Found, error 404.
+            <Header />
+            <h1>
+                Not Found, error 404. <strong>:(</strong>
+            </h1>
         </div>
     );
 }
