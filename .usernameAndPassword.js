376d4d51764fa9f7b989768a142cb22ddc876f95
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-18 11:55:29 -0500

    add: file to use as database for users

---
+++ .usernameAndPassword.js
@@ -0,0 +1,20 @@
+const userDB = [
+    {
+        "email": "juan@email.com", 
+        "password": "contrasena123"
+    },
+    {
+        "email": "luis@email.com",
+        "password": "contrasena456"
+    },
+    {
+        "email": "john@email.com",
+        "password": "contrasena789"
+    },
+    {
+        "email": "stan@email.com",
+        "password": "contrasena101112"
+    }
+];
+
+export default userDB;
