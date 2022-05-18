16c6ba244c642d7a7395c5e4afa0cdfad9f8287c
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-18 16:43:59 -0500

    add: name to user objects

--- .usernameAndPassword.js
+++ .usernameAndPassword.js
@@ -1,17 +1,21 @@
 const userDB = [
     {
+        "name": "Juan Lopez",
         "email": "juan@email.com", 
         "password": "contrasena123"
     },
     {
+        "name": "Luis Perez",
         "email": "luis@email.com",
         "password": "contrasena456"
     },
     {
+        "name": "John Doe",
         "email": "john@email.com",
         "password": "contrasena789"
     },
     {
+        "name": "Stan Lee",
         "email": "stan@email.com",
         "password": "contrasena101112"
     }
