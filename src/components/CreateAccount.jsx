61e3aef7b9918369ea8ee4395e931ea25f608f9c
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-19 11:05:55 -0500

    add: configuration to obtain info from new users

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 import './../styles/createAccount.scss';
+import UserContext from "../context/UserContext";
 
 const CreateAccount = () => {
+    
+    const form = useRef(null);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(form.current);
+        const data = {
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password'),
+        }
+        console.log(data);
+    }
+
     return(
         <div className="login">
             <div className="form-container">
                 <h1 className="title">My account</h1>
-                <form action="/" className="form">
+                <form action="/" className="form" ref={form}>
                     <div>
                     <label htmlFor="name" className="label">Name</label>
-                    <input type="text" id="name" placeholder="Teff" className="input input-name" />
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Bruno" 
+                        className="input input-name" />
                     <label htmlFor="email" className="label">Email</label>
-                    <input type="text" id="email" placeholder="correo@example.com" className="input input-email" />
+                    <input 
+                        type="text" 
+                        name="email" 
+                        placeholder="correo@example.com" 
+                        className="input input-email" />
                     <label htmlFor="password" className="label">Password</label>
-                    <input type="password" id="password" placeholder="*********" className="input input-password" />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="*********" 
+                        className="input input-password" />
                     </div>
-                    <input type="submit" value="Create" className="primary-button login-button" />
+                    <input type="submit" value="Create" className="primary-button login-button" onClick={handleSubmit}/>
                 </form>
             </div>
         </div>
